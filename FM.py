fc383b1c1c4081a257281c7d761cae5c6fd0eccd
mitsuha0258 <2660316551@qq.com>
2024-03-27 14:34:25 +0800

    完成FM模型

--- FM.py
+++ FM.py
@@ -1,8 +1,10 @@
+import numpy as np
 import torch
 import torch.nn as nn
+from tqdm import tqdm
 from torch.utils.data import random_split, DataLoader
 
-from utils import CretioDataset
+from utils import CretioDataset, EarlyStopper
 
 
 class FM(nn.Module):
@@ -13,7 +15,7 @@ class FM(nn.Module):
 
         self.linear = nn.Linear(self.feats_num, 1)
         self.v = nn.Parameter(torch.rand(self.feats_num, self.hidden_dim))
-        nn.init.xavier_uniform_(self.v.data.weight)
+        nn.init.xavier_uniform_(self.v)
 
     def forward(self, x):
         first_order = self.linear(x)
@@ -25,7 +27,44 @@ class FM(nn.Module):
         return torch.sigmoid(first_order + second_order)
 
 
+def train(model, optimizer, criterion, train_loader, device):
+    model.train()
+    loop = tqdm(train_loader)
+    for i, (x, label) in enumerate(loop):
+        optimizer.zero_grad()
+
+        x, label = x.to(device), label.to(device)
+        output = model(x)
+        loss = criterion(output.squeeze(), label.float())
+        loss.backward()
+
+        optimizer.step()
+
+        loop.set_postfix(loss=loss.item())
+
+
+def test(model, data_loader, device):
+    model.eval()
+    correct, total_num = 0, 0
+    with torch.no_grad():
+        for x, label in tqdm(data_loader):
+            x, label = x.to(device), label.to(device)
+            output = model(x)
+            output = torch.where(output >= 0.5, 1, 0).squeeze()
+            correct += (output == label).cpu().sum().item()
+            total_num += len(label)
+    return correct / total_num
+
+
 if __name__ == '__main__':
+    save_path = 'saved_model/FM.pt'
+    device = 'cuda:0'
+    batch_size = 64
+    hidden_dim = 16
+    learning_rate = 1e-3
+    weight_decay = 1e-4
+    epochs = 100
+
     dataset = CretioDataset()
     train_lens, val_lens = int(len(dataset) * 0.6), int(len(dataset) * 0.2)
     test_lens = len(dataset) - train_lens - val_lens
@@ -34,3 +73,19 @@ if __name__ == '__main__':
     train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
     val_loader = DataLoader(val_set, batch_size=64, shuffle=True)
     test_loader = DataLoader(test_set, batch_size=64, shuffle=True)
+
+    model = FM(dataset.feats_num, hidden_dim=16).cuda()
+    criterion = torch.nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    early_stopper = EarlyStopper(patience=10, save_path=save_path)
+
+    print("Start Training...")
+    for epoch in range(1, epochs + 1):
+        train(model, optimizer, criterion, train_loader, device)
+        acc = test(model, val_loader, device)
+        print(f'epoch: {epoch}, val acc: {acc: .4f}')
+        if not early_stopper.is_continuable(model, acc):
+            print(f'val best acc: {early_stopper.best_acc: .4f}')
+            break
+    acc = test(model, test_loader, device)
+    print(f'test acc: {acc}')
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import torch
 from torch.utils.data import Dataset
 
 
@@ -7,6 +9,10 @@ class CretioDataset(Dataset):
         self.data = pd.read_csv('dataset/cretio/preprocessed/cretio_data.csv', header=0)
         self.labels = self.data['Label']
         del self.data['Label']
+        self.feats_num = self.data.shape[1]
+
+        self.data = torch.from_numpy(self.data.values).float()
+        self.labels = torch.LongTensor(np.array(self.labels))
 
     def __getitem__(self, idx):
         return self.data[idx], self.labels[idx]
@@ -14,3 +20,22 @@ class CretioDataset(Dataset):
     def __len__(self):
         return len(self.labels)
 
+
+class EarlyStopper(object):
+    def __init__(self, patience, save_path):
+        self.patience = patience
+        self.counter = 1
+        self.best_acc = 0.0
+        self.save_path = save_path
+
+    def is_continuable(self, model, acc):
+        if acc > self.best_acc:
+            self.best_acc = acc
+            self.counter = 1
+            torch.save(model, self.save_path)
+            return True
+        elif self.counter < self.patience:
+            self.counter += 1
+            return True
+        else:
+            return False
